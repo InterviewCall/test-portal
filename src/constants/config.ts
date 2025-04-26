07617f4fb3a1ed78f567f67526a77fc842c4b44a
Arijit Ganguly <aganguly158@gmail.com>
2025-04-27 01:29:55 +0530

    siteconfig updated

---
+++ src/constants/config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+    title: 'InterviewCall Test Portal',
+    description:
+      'Crush your test, and we’ll match you with the perfect course and batch designed to level up your skills — so you can confidently crack FAANG and other top tech giants.',
+    /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
+    url: 'https://www.test-portal.interviewcall.club',
+};
